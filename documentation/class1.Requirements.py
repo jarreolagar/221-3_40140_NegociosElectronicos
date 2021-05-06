387a4b0f2bf8bd28d0c6fd0a618f5659b3154cc1
Juan Arreola <jmarreola.gar@outlook.com>
2021-05-06 17:01:45 -0500

    Project requirements and installation of flask and vue

---
+++ documentation/class1.Requirements.py
@@ -0,0 +1,17 @@
+project_technologies = "Python + Flask + Bootstrap 4 / VueJS"
+project_description = f"Create a Web Business app with {project_technologies}"
+
+print(project_description)
+
+project_delirables_one = "Analysis and design phases (project documentation)"
+project_delirables_two = "Business app structure (flask + MySQL)"
+project_delirables_three = "VueJS"
+
+project_delirables_date_one = "Wednesday June 4th, 2021"
+project_delirables_date_two = "Wednesday July 7th, 2021"
+project_delirables_date_three = "Wednesday August 4th, 2021"
+
+project_objective = "What is my business application about"
+
+for_backend = "Flask 1.1.2"
+for_frontend = "node, npm, vue, vuecli"
