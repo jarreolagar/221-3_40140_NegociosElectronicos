e78fe7e8972c0a683a0d16020dbb8329bfb2b580
Juan Arreola <jmarreola.gar@outlook.com>
2021-05-12 20:00:50 -0500

    setting up frontend with VueJs

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data(){
+        return{
+            message: "Learning VueJs 2021, with Luis Guerra & Juanito"
+        }
+    }
+}
+</script>
+    
